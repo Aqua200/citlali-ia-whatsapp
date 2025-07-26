49e45769d0f6d4fdb7d37de0713418e23f6e9985
Neykoor💜 <chinquepapa@gmail.com>
2025-07-26 00:07:12 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-import { connect } from './src/lib/connect.js'; 
-
-import { handleMessage } from './src/bot/handleMessage.js'; 
+import { connect } from './src/lib/connect.js';
+import config, { resolveGroupJIDs } from './src/lib/config.js';
+import { handleMessage } from './src/bot/handleMessage.js';
 
 async function start() {
   const sock = await connect();
+  await resolveGroupJIDs(sock);
 
   sock.ev.on('messages.upsert', async ({ messages }) => {
     const msg = messages[0];
     if (!msg?.message) return;
-
     await handleMessage(sock, msg);
   });
 
