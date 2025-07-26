986c46830fd4995f86ec529fe5563a0f9aa77c70
Neykoor💜 <chinquepapa@gmail.com>
2025-07-26 02:57:31 -0700

    Update config.js

--- src/lib/config.js
+++ src/lib/config.js
@@ -4,14 +4,19 @@ export async function resolveGroupJIDs(sock) {
       const groupInviteCode = config.officialGroup.split('/').pop();
       const groupInfo = await sock.groupGetInviteInfo(groupInviteCode);
       config.officialGroupJID = groupInfo.id;
+      console.log(`✅ JID del grupo resuelto: ${config.officialGroupJID}`);
     }
   } catch (e) {
-    console.error(`Error al resolver JID del grupo:`, e.message);
+    console.error(`❌ No se pudo resolver JID del grupo:`, e.message);
   }
 }
 
 const config = {
-  owner: ['5211234567890@s.whatsapp.net'],
+  owner: [
+    ['5216631079388', '🜲 Propietario 🜲', true],
+    ['5212202410659', 'Co-Dueño', false],
+    ['573154062343', 'Colaborador', false],
+  ],
   botAge: 11,
   officialGroup: 'https://chat.whatsapp.com/JchA8tqCFPLCRbG8aKIF2e',
   officialChannel: 'https://whatsapp.com/channel/0029VbAZ6vWF1YlYsbKCrx1k',
