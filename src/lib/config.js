fc643ed40d51ff2cda4608af3d804d23e723f81e
Neykoor💜 <chinquepapa@gmail.com>
2025-07-26 00:54:32 -0700

    Update config.js

--- src/lib/config.js
+++ src/lib/config.js
@@ -2,8 +2,8 @@ const config = {
     owner: [
         '5211234567890@s.whatsapp.net'
     ],
-    officialGroup: 'https://chat.whatsapp.com/TU_ENLACE_DE_GRUPO',
-    officialChannel: 'https://whatsapp.com/channel/TU_ENLACE_DE_CANAL',
+    officialGroup: '',
+    officialChannel: '',
     botAge: 11,
     officialGroupJID: null,
     officialChannelJID: null,
