60051680b4626f2c2d52d7ab0ed3c4da61ba0cb8
Neykoor💜 <chinquepapa@gmail.com>
2025-07-26 15:35:56 -0700

    Create internalMonologue.js

---
+++ src/bot/internalMonologue.js
@@ -0,0 +1,25 @@
+Aquí tienes el código sin los comentarios:
+
+import chalk from 'chalk';
+import { getRandomThought } from '../lib/thoughts.js';
+
+function printThought(thought) {
+    console.log(chalk.magenta('-----------------------------------------'));
+    console.log(chalk.magenta.bold('💭 Pensamiento Interno de Citlali:'));
+    console.log(chalk.magenta(`   "${thought}"`));
+    console.log(chalk.magenta('-----------------------------------------'));
+}
+
+export function startInternalMonologue() {
+    console.log(chalk.cyan('✨ La mente de Citlali ha despertado.'));
+
+    const think = () => {
+        const thought = getRandomThought();
+        printThought(thought);
+        const nextThoughtDelay = Math.floor(Math.random() * (300000 - 60000 + 1)) + 60000;
+        setTimeout(think, nextThoughtDelay);
+    };
+
+    setTimeout(think, 10000);
+}
+
