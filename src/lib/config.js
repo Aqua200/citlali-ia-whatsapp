123a46b1b275bfba82442a6cd1df0b42e19919ff
Neykoor💜 <chinquepapa@gmail.com>
2025-07-26 00:59:56 -0700

    Update config.js

--- src/lib/config.js
+++ src/lib/config.js
@@ -2,8 +2,8 @@ const config = {
     owner: [
         '5211234567890@s.whatsapp.net'
     ],
-    officialGroup: '',
-    officialChannel: '',
+    officialGroup: 'https://chat.whatsapp.com/JchA8tqCFPLCRbG8aKIF2e?mode=ac_t',
+    officialChannel: 'https://whatsapp.com/channel/0029VbAZ6vWF1YlYsbKCrx1k',
     botAge: 11,
     officialGroupJID: null,
     officialChannelJID: null,
