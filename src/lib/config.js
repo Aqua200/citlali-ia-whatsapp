21d990f1149ff447e9f23034311f6873a9035f10
Neykoor💜 <chinquepapa@gmail.com>
2025-07-26 02:35:58 -0700

    Update config.js

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,10 +1,22 @@
+export async function resolveGroupJIDs(sock) {
+  try {
+    if (config.officialGroup && config.officialGroup.startsWith('https://chat.whatsapp.com/')) {
+      const groupInviteCode = config.officialGroup.split('/').pop();
+      const groupInfo = await sock.groupGetInviteInfo(groupInviteCode);
+      config.officialGroupJID = groupInfo.id;
+    }
+  } catch (e) {
+    console.error(`Error al resolver JID del grupo:`, e.message);
+  }
+}
+
 const config = {
-    owner: [
-        '5211234567890@s.whatsapp.net'
-    ],
-    botAge: 11,
-    officialGroup: 'https://chat.whatsapp.com/TU_ENLACE',
-    officialChannel: 'https://whatsapp.com/channel/TU_ENLACE',
+  owner: ['5211234567890@s.whatsapp.net'],
+  botAge: 11,
+  officialGroup: 'https://chat.whatsapp.com/JchA8tqCFPLCRbG8aKIF2e',
+  officialChannel: 'https://whatsapp.com/channel/0029VbAZ6vWF1YlYsbKCrx1k',
+  officialGroupJID: null,
+  officialChannelJID: null,
 };
 
 export default config;
